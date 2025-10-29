75b0d8239676ad6f7ddf2fd8a7d171d9910a01db
k1rie <d628587@gmail.com>
2025-10-28 20:07:47 -0600

    api-production

--- index.js
+++ index.js
@@ -61,3 +61,49 @@ app.post('/getMaximization', (req, res) => {
         iterations: zArray
     });
 });
+
+app.post('/getMinimization', (req, res) => {
+    const regex = /obj\s*=\s*([-\d.e+]+)/i;
+
+    const options = {
+        msglev: glpk.GLP_MSG_ALL,
+        presol: false,
+        cb: {
+            call: progress => console.log(progress),
+            each: 1
+        }
+    };
+
+    let glpkIterations = null;
+    process.stdout.write = (chunk, encoding, callback) => {
+        glpkIterations += chunk.toString();
+        if (callback) callback();
+        return true;
+    };
+
+    const result = glpk.solve({
+        name: 'LP',
+        objective: {
+            direction: glpk.GLP_MIN,
+            name: 'obj',
+            vars: req.body.objective
+        },
+        subjectTo: req.body.constraints
+    }, options);
+
+    process.stdout.write = process.stdout.write;;
+
+    const zArray = glpkIterations
+  .split('\n')
+  .map(line => line.match(regex))
+  .filter(match => match)
+  .map(match => parseFloat(match[1]));
+
+    console.log('GLPK Result:', result);
+    console.log('GLPK Iterations:', glpkIterations);
+    res.json({
+        message: 'Minimization completed',
+        result: result,
+        iterations: zArray
+    });
+});
